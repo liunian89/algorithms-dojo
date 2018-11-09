8bd24cc5a4904833082bc42facb93bc8e2dce44f
Nian Liu <nliu@bol.com>
2018-11-09 21:53:36 +0100

    init commit

---
+++ src/main/kotlin/com/liunian/algorithmsdojo/AlgorithmsDojoApplication.kt
@@ -0,0 +1,11 @@
+package com.liunian.algorithmsdojo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgorithmsDojoApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlgorithmsDojoApplication>(*args)
+}
